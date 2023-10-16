5e83bfa23277bcc6fc7143e9414dadff83193cd0
ivkozlov <ivkozlov@ozon.ru>
2023-10-16 16:20:32 +0200

    make folder

---
+++ emm/example/script.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+from emm import EMM
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('data/german-credit-scoring.csv', sep=";")
+    # df['Credit amount'], _ = pd.cut(df['Credit amount'], bins=10, retbins=True)
+    # df['Credit amount'] = df['Credit amount'].apply(lambda x: f"{int(x.left)} - {int(x.right)}")
+    df['Credit amount'] = df['Credit amount'].astype(int)
+    df['Duration in months'] = df['Duration in months'].astype(int)
+    clf = EMM(width=50, depth=2, evaluation_metric='heatmap', n_jobs=-1)
+    clf.search(df, target_cols=['Score', 'Savings account/bonds'])
+    clf.visualise(cols=2, subgroups=3, include_dataset=True)
+
